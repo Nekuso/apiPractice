93cb82ea064528ea7d28648461779c097bd98b15
Nixxo <92588172+Nekuso@users.noreply.github.com>
2023-05-08 13:32:29 +0800

    Updated

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\AuthController;
 // Route::resource('/products', ProductController::class);
 
 //Public Routes
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
